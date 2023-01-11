27756450f6a725fcf4cea9b7652108b1ad66f6e9
BRNSCH <chrischmitt@hotmail.com>
2023-01-11 15:57:50 -0300

    final

--- server.js
+++ server.js
@@ -20,62 +20,33 @@ const db = knex ({
 const app = express();
 app.use(express.json());
 app.use(cors())
-const database = {
-    users: [
-        {
-            id: "123",
-            name: "John",
-            email: 'john@gmail.com',
-            password: "bolos",
-            entries: 0,
-            joined: new Date()
-        },
-        {
-            id: "124",
-            name: "Jane",
-            email: 'jane@gmail.com',
-            password: "tortas",
-            entries: 0,
-            joined: new Date()
-        },
-    ],
-    login: [
-        {
-            id: '080',
-            hash: '',
-            email:'john@gmail.com'
-        }
-    ]
-}
+
 
 app.get('/', (req, res) => {
     res.send(database.users)
 })
 
 app.post('/signin', (req, res) => {   
+    db.select('email', 'hash').from('login')
+        .where('email', '=',  req.body.email)
+        .then(data => {
+            const isValid = bcrypt.compareSync(req.body.password, data[0].hash); // true
+            
+            if (isValid) {
+                return db.select("*").from('users')
+                .where('email', '=', req.body.email)
+                .then(user => {
+                    
+                    res.json(user[0])
+                })
+                .catch(err => res.status(400).json('unable to get user'))
+            } else {
+                res.status(400).json('wrong credentials')
+            }
+           
+        })
+   .catch(err => res.status(400).json('wrong credentials'))
     
-    bcrypt.compare("cafe",
-        '$2a$10$MtuCmGc/hLzBEpzQG3wWjuv1iB.GE5XoJcvO4vbsYl6VeN9y.VXCC',
-        function (err, res) {
-            // res == true
-        console.log('first guess', res)
-        });
-    bcrypt.compare("veggies",
-        '$2a$10$MtuCmGc/hLzBEpzQG3wWjuv1iB.GE5XoJcvO4vbsYl6VeN9y.VXCC',
-        function (err, res) {
-        // res = false
-        console.log('second guess', res)
-    });
-    if (
-        req.body.email === database.users[0].email
-        &&
-        req.body.password === database.users[0].password
-    ) {
-        res.json(database.users[0]);
-    } else {
-        res.status(400).json('Error logging in')
-    }
-    res.json('signin')
 })
 
 app.post('/register', (req, res) => { 
