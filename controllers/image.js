ccaccddbc340948531401ccc3f37efb13c35e2f9
BRNSCH <chrischmitt@hotmail.com>
2023-01-11 20:36:49 -0300

    api

--- controllers/image.js
+++ controllers/image.js
@@ -1,4 +1,19 @@
-const handleImage=(req, res,db) => {
+const Clarifai = require('clarifai');
+
+
+const app = new Clarifai.App({
+    apiKey: 'd3f71972eaf4457db620b1605e92facd'
+});
+
+const handleApiCall = (req, res) => {
+    app.models
+        .predict(Clarifai.CELEBRITY_MODEL, req.body.input)
+        .then(data => {
+        res.json(data);
+        })
+        .catch(err => res.status(400).json('unable to work with API'));
+}
+const handleImage = (req, res, db) => {
     const { id } = req.body;
 
     db('users').where('id', '=', id)
@@ -11,5 +26,6 @@ const handleImage=(req, res,db) => {
 }
 
 module.exports = {
-    handleImage
+    handleImage,
+    handleApiCall
 }
--- server.js
+++ server.js
@@ -2,7 +2,6 @@ const express = require('express');
 const bcrypt = require('bcrypt-nodejs');
 const cors = require('cors');
 const knex = require('knex');
-
 const register = require('./controllers/register');
 const signin = require('./controllers/signin');
 const profile = require('./controllers/profile');
@@ -32,7 +31,8 @@ app.use(cors())
 app.post('/signin', signin.handleSignin(db,bcrypt) )
 app.post('/register',(req,res) =>{register.handleRegister(req,res,db,bcrypt)})
 app.get('/profile/:id',(req,res) =>{profile.handleProfileGet(req,res,db)})
-app.put('/image', (req, res) => {image.handleImage(req,res,db)})
+app.put('/image', (req, res) => { image.handleImage(req, res, db) })
+app.post('/imageurl', (req, res) => { image.handleApiCall(req, res) })
     
 // bcrypt.hash("bacon", null, null, function (err, hash) {
 //     // Store hash in your password DB.
